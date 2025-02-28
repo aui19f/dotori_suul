7a2f98f362f6abb3b8d9031a33cb80709bcd3fc6
aui19f <aui19f@gmail.com>
2025-02-28 21:19:02 +0900

    fbase 수정

--- src/fbase.ts
+++ src/fbase.ts
@@ -19,8 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
